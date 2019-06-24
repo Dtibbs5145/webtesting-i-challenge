610af59852922d921468198c7d586cd154e8b906
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-06-24 18:57:13 -0400

    I added in the repair() and am now working on succeed()

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,19 @@
-const enhancer = require('./enhancer.js');
+const { repair, succeed, fail } = require('./enhancer.js');
+
+describe('enhancer.js', () => {
+    describe('repair()', () => {
+        it('should reset durability too 100', () => {
+            expect(repair({ durability: 100 }).durability).toBe(100),
+            expect(repair({ durability: 15 }).durability).toBe(100), 
+            expect(repair({ durability: 98 }).durability).toBe(100), 
+            expect(repair({ durability: 1 }).durability).toBe(100)
+        });
+    });
+    describe('succeed()', () => {
+        it('')
+    })
+
+
+
+})
 // test away!
